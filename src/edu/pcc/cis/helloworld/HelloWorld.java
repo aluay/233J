a58657606b3e89a8555aa2eb1ba7d6274d3b4010
Abdullah Luay <aluay95@gmail.com>
2020-04-27 22:14:08 -0700

    Added my name as an author and the date the project was created.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,6 +1,11 @@
 package edu.pcc.cis.helloworld;
 import java.util.Date;
 
+/**
+ * Author: Abdullah Luay
+ * Created on 4/27/2020
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
